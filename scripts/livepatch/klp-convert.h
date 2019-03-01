d7f4d266586f9ad6a846139171e9fc0e560656cf
Josh Poimboeuf <jpoimboe@redhat.com>
2019-03-01 11:13:08 -0300

    livepatch: Add klp-convert tool
    Livepatches may use symbols which are not contained in its own scope,
    and, because of that, may end up compiled with relocations that will
    only be resolved during module load. Yet, when the referenced symbols
    are not exported, solving this relocation requires information on the
    object that holds the symbol (either vmlinux or modules) and its
    position inside the object, as an object may contain multiple symbols
    with the same name. Providing such information must be done
    accordingly to what is specified in
    Documentation/livepatch/module-elf-format.txt.
    
    Currently, there is no trivial way to embed the required information
    as requested in the final livepatch elf object. klp-convert solves
    this problem in two different forms: (i) by relying on Symbols.list,
    which is built during kernel compilation, to automatically infer the
    relocation targeted symbol, and, when such inference is not possible
    (ii) by using annotations in the elf object to convert the relocation
    accordingly to the specification, enabling it to be handled by the
    livepatch loader.
    
    Given the above, create scripts/livepatch to hold tools developed for
    livepatches and add source files for klp-convert there.
    
    The core file of klp-convert is scripts/livepatch/klp-convert.c, which
    implements the heuristics used to solve the relocations and the
    conversion of unresolved symbols into the expected format, as defined
    in [1].
    
    klp-convert receives as arguments the Symbols.list file, an input
    livepatch module to be converted and the output name for the converted
    livepatch. When it starts running, klp-convert parses Symbols.list and
    builds two internal lists of symbols, one containing the exported and
    another containing the non-exported symbols. Then, by parsing the rela
    sections in the elf object, klp-convert identifies which symbols must
    be converted, which are those unresolved and that do not have a
    corresponding exported symbol, and attempts to convert them
    accordingly to the specification.
    
    By using Symbols.list, klp-convert identifies which symbols have names
    that only appear in a single kernel object, thus being capable of
    resolving these cases without the intervention of the developer. When
    various homonymous symbols exist through kernel objects, it is not
    possible to infer the right one, thus klp-convert falls back into
    using developer annotations. If these were not provided, then the tool
    will print a list with all acceptable targets for the symbol being
    processed.
    
    Annotations in the context of klp-convert are accessible as struct
    klp_module_reloc entries in sections named
    .klp.module_relocs.<objname>. These entries are pairs of symbol
    references and positions which are to be resolved against definitions
    in <objname>.
    
    Define the structure klp_module_reloc in
    include/linux/uapi/livepatch.h allowing developers to annotate the
    livepatch source code with it.
    
    klp-convert relies on libelf and on a list implementation. Add files
    scripts/livepatch/elf.c and scripts/livepatch/elf.h, which are a
    libelf interfacing layer and scripts/livepatch/list.h, which is a
    list implementation.
    
    Update Makefiles to correctly support the compilation of the new tool,
    update MAINTAINERS file and add a .gitignore file.
    
    [1] - Documentation/livepatch/module-elf-format.txt
    
    [khlebnikov: use HOSTLOADLIBES_ instead of HOSTLDFLAGS: -lelf must be
    at the end]
    [jmoreira:
    * add support to automatic relocation conversion in klp-convert.c
    * changelog]
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Signed-off-by: Joao Moreira <jmoreira@suse.de>

--- scripts/livepatch/klp-convert.h
+++ scripts/livepatch/klp-convert.h
@@ -1,7 +1,6 @@
 /*
- * livepatch.h - Kernel Live Patching Core
- *
  * Copyright (C) 2016 Josh Poimboeuf <jpoimboe@redhat.com>
+ * Copyright (C) 2017 Joao Moreira   <jmoreira@suse.de>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -17,17 +16,35 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _UAPI_LIVEPATCH_H
-#define _UAPI_LIVEPATCH_H
+#define SHN_LIVEPATCH		0xff20
+#define SHF_RELA_LIVEPATCH	0x00100000
+#define MODULE_NAME_LEN		(64 - sizeof(GElf_Addr))
+#define WARN(format, ...) \
+	fprintf(stderr, "klp-convert: " format "\n", ##__VA_ARGS__)
 
-#include <linux/types.h>
+/*
+ * klp-convert uses macros defined in the linux sources package. To prevent the
+ * dependency when building locally, they are defined below. Also notice that
+ * these should match the definitions from  the targeted kernel.
+ */
 
 #define KLP_RELA_PREFIX		".klp.rela."
 #define KLP_SYM_PREFIX		".klp.sym."
+
+struct symbol_entry {
+	struct list_head list;
+	char *symbol_name;
+	char *object_name;
+};
+
+struct sympos {
+	struct list_head list;
+	char *symbol_name;
+	char *object_name;
+	int pos;
+};
 
 struct klp_module_reloc {
 	void *sym;
 	unsigned int sympos;
 } __attribute__((packed));
-
-#endif /* _UAPI_LIVEPATCH_H */
