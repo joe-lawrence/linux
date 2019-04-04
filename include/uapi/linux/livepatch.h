cf470830318660e549b7eb22c51c7569c0c2dd68
Josh Poimboeuf <jpoimboe@redhat.com>
2019-04-04 15:50:44 -0400

    livepatch: Create and include UAPI headers
    Define klp prefixes in include/uapi/linux/livepatch.h, and use them for
    replacing hard-coded values in kernel/livepatch/core.c.
    
    Update MAINTAINERS.
    
    Note: Add defines to uapi as these are also to be used by a newly
    introduced klp-convert script.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Joao Moreira <jmoreira@suse.de>
    Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>

---
+++ include/uapi/linux/livepatch.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/*
+ * livepatch.h - Kernel Live Patching Core
+ *
+ * Copyright (C) 2016 Josh Poimboeuf <jpoimboe@redhat.com>
+ */
+
+#ifndef _UAPI_LIVEPATCH_H
+#define _UAPI_LIVEPATCH_H
+
+#include <linux/types.h>
+
+#define KLP_RELA_PREFIX		".klp.rela."
+#define KLP_SYM_PREFIX		".klp.sym."
+
+#endif /* _UAPI_LIVEPATCH_H */
