f0d02eceb1e52837b74eda3f237f972a18507c6b
Josh Poimboeuf <jpoimboe@redhat.com>
2020-03-24 17:39:03 -0400

    modpost: Integrate klp-convert
    Create cmd_klp_convert and hook it into scripts/Makefile.modpost.
    cmd_klp_convert invokes klp-convert with the right arguments for the
    conversion of unresolved symbols inside a livepatch.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Signed-off-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Joao Moreira <jmoreira@suse.de>
    Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -2041,6 +2041,10 @@ static void read_symbols(const char *modname)
 		namespace = get_next_modinfo(&info, "import_ns", namespace);
 	}
 
+	/* Livepatch modules have unresolved symbols resolved by klp-convert */
+	if (get_modinfo(&info, "livepatch"))
+		mod->livepatch = 1;
+
 	for (sym = info.symtab_start; sym < info.symtab_stop; sym++) {
 		symname = remove_dot(info.strtab + sym->st_name);
 
@@ -2197,7 +2201,7 @@ static int check_exports(struct module *mod)
 		const char *basename;
 		exp = find_symbol(s->name);
 		if (!exp || exp->module == mod) {
-			if (have_vmlinux && !s->weak) {
+			if (have_vmlinux && !s->weak && !mod->livepatch) {
 				if (warn_unresolved) {
 					warn("\"%s\" [%s.ko] undefined!\n",
 					     s->name, mod->name);
