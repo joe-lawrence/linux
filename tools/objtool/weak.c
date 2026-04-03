b2218e5970ed6d62b592a261136c4ff213ae925f
Josh Poimboeuf <jpoimboe@kernel.org>
2026-04-03 11:57:02 -0700

    objtool/klp: Fix position-dependent checksums for non-relocated jumps/calls
    When computing klp checksums, instructions with non-relocated jump/call
    destination offsets are problematic because the offset values can change
    when surrounding code has moved, causing the function to be incorrectly
    marked as changed.
    
    Specifically, that includes jumps from alternatives to the end of the
    alternative, which from objtool's perspective are jumps to to the end of
    the alternative instruction block in the original function.
    
    Note that 'jump_dest' jumps dont include sibling calls (those use
    call_dest), nor does they include jumps to/from .cold sub functions
    (those are cross-section and need a reloc).
    
    Fix it by hashing the opcode bytes (excluding the immediate operand)
    along with a position-independent representation of the destination.
    For calls, use the function name, and for jumps, use the destination's
    offset within its function.
    
    Note the existing "9 bit hole" comment is wrong since commit
    70589843b36f ("objtool: Add option to trace function validation").  Thus
    there's only a 4 bit hole now despite only adding 4 bytes.
    
    Fixes: 0d83da43b1e1 ("objtool/klp: Add --checksum option to generate per-function checksums")
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>

--- tools/objtool/weak.c
+++ tools/objtool/weak.c
@@ -7,8 +7,10 @@
 
 #include <stdbool.h>
 #include <errno.h>
+#include <string.h>
 #include <objtool/objtool.h>
 #include <objtool/arch.h>
+#include <objtool/check.h>
 #include <objtool/builtin.h>
 
 #define UNSUPPORTED(name)						\
@@ -30,4 +32,14 @@ int __weak orc_create(struct objtool_file *file)
 int __weak cmd_klp(int argc, const char **argv)
 {
 	UNSUPPORTED("klp");
+}
+
+void __weak arch_jump_opcode_bytes(struct objtool_file *file,
+				   struct instruction *insn,
+				   unsigned char *buf, size_t *len)
+{
+	/*
+	 * Nothing to see here.  This has no purpose other than to avoid
+	 * breaking bisection builds.  It will be removed shortly.
+	 */
 }
